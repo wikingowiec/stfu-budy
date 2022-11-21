bb7a902dab7c564cfe6bb8c0f9a93af16f808457
wikingowiec <Wiktor.wystrychowski@gmail.com>
2022-11-21 13:36:34 +0100

    score

--- Assets/Assets/Rhythm Game Tutorial/scripts/GameManager.cs
+++ Assets/Assets/Rhythm Game Tutorial/scripts/GameManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI; 
 
 public class GameManager : MonoBehaviour
 {
@@ -11,11 +12,22 @@ public class GameManager : MonoBehaviour
     public BeatScroller theBS;
 
     public static GameManager instance;
+
+    public int currentMultiplier;
+    public int multilpierTracker; 
+    public int[] multiplierThresholds; 
+
+    public int currentScore;
+    public int scorePerNote = 100; 
     
+    public Text scoreText;
+    public Text multiText; 
     // Start is called before the first frame update
     void Start()
     {
         instance = this;
+        scoreText.text = "score: 0";
+        currentMultiplier = 1; 
     }
 
     // Update is called once per frame
@@ -35,10 +47,29 @@ public class GameManager : MonoBehaviour
      public void NoteHit()
      {
         Debug.Log("Hit On Time");
+
+        if(currentMultiplier - 1 <multiplierThresholds.Length)
+        {
+        multilpierTracker ++;
+        if(multiplierThresholds[currentMultiplier -1]<= multilpierTracker)
+        {
+            multilpierTracker = 0;
+            currentMultiplier++;
+        }
+        }
+
+        multiText.text = "Multiplier: x" + currentMultiplier;
+        currentScore += scorePerNote * currentMultiplier;
+        scoreText.text = "Score:  " + currentScore; 
      }
 
      public void NoteMissed()
      {
         Debug.Log("Missed Note");
+
+       // currentMultiplier = 1;
+       // multilpierTracker = 0;
+
+       //  multiText.text = "Multiplier: x" + currentMultiplier; *
      }
 }
