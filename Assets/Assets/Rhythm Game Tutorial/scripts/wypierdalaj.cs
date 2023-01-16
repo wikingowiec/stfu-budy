6ab5c3cfffb4296ca2fce6026edf0f0835556af7
wikingowiec <113859465+wikingowiec@users.noreply.github.com>
2023-01-16 17:27:11 +0100

    234567890poiuytrewqasdfghjklkmnbvcxsedcvfrtgbnhy

---
+++ Assets/Assets/Rhythm Game Tutorial/scripts/wypierdalaj.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class wypierdalaj : MonoBehaviour
+{
+   
+    public void doExitGame() {
+     Application.Quit();
+ }
+}
